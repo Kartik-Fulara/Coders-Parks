94adb85d9623904a8e54be530e819fe195ad7935
Kartik Fulara <kartikfulara87@gmail.com>
2022-10-31 10:48:58 +0530

    remove something

--- utils/createRoomID.tsx
+++ utils/createRoomID.tsx
@@ -1,7 +1,9 @@
-import { v4 as uuidv4 } from "uuid";
+import React from 'react'
 
 const createRoomID = () => {
-  return uuidv4();
-};
+  return (
+    <div>createRoomID</div>
+  )
+}
 
-export default createRoomID;
+export default createRoomID
